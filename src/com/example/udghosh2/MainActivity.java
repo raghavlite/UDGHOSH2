f8ca8317cfdea3c49140dd70b72dc51a8c6956ff
raghavlite <raghavlite@gmail.com>
2014-06-23 01:13:59 +0530

    made changes in sidenavigation library and it's getting close to fest app,cheers

--- src/com/example/udghosh2/MainActivity.java
+++ src/com/example/udghosh2/MainActivity.java
@@ -1,5 +1,14 @@
 package com.example.udghosh2;
 
+import java.util.ArrayList;
+
+import com.devspark.sidenavigation.ISideNavigationCallback;
+import com.devspark.sidenavigation.SideNavigationView;
+import com.devspark.sidenavigation.SideNavigationView.Mode;
+
+
+
+import android.R.integer;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -9,21 +18,29 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 import android.os.Build;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements ISideNavigationCallback{
 
+	
+	private SideNavigationView sideNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_main);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
+        sideNavigationView = (SideNavigationView) findViewById(R.id.side_navigation_view);
+        ArrayList<Integer> a=new ArrayList<Integer>();
+        a.add(1);
+        a.add(2);
+        a.add(4);
         
+        sideNavigationView.setHeads(a);
+        
+        sideNavigationView.setMenuItems(R.menu.main);
+        sideNavigationView.setMenuClickCallback(this);
+        sideNavigationView.setMode(Mode.LEFT);
         
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         
@@ -44,7 +61,9 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == android.R.id.home) {
+        	
+        	sideNavigationView.toggleMenu();
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -64,6 +83,41 @@ public class MainActivity extends ActionBarActivity {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
         }
+    }
+
+    @Override
+    public void onSideNavigationItemClick(int itemId) {
+        switch (itemId) {
+            case R.id.side_navigation_menu_item1:
+               
+            	Toast.makeText(getApplicationContext(), "1", Toast.LENGTH_SHORT).show();
+            	// invokeActivity(getString(R.string.title1), R.drawable.ic_android1);
+                break;
+
+            case R.id.side_navigation_menu_item2:
+            	Toast.makeText(getApplicationContext(), "2", Toast.LENGTH_SHORT).show();
+            	//invokeActivity(getString(R.string.title2), R.drawable.ic_android2);
+                break;
+
+            case R.id.side_navigation_menu_item3:
+            	Toast.makeText(getApplicationContext(), "3", Toast.LENGTH_SHORT).show();
+                //invokeActivity(getString(R.string.title3), R.drawable.ic_android3);
+                break;
+
+            case R.id.side_navigation_menu_item4:
+            	Toast.makeText(getApplicationContext(), "4", Toast.LENGTH_SHORT).show();
+                //invokeActivity(getString(R.string.title4), R.drawable.ic_android4);
+                break;
+
+            case R.id.side_navigation_menu_item5:
+            	Toast.makeText(getApplicationContext(), "5", Toast.LENGTH_SHORT).show();
+                //invokeActivity(getString(R.string.title5), R.drawable.ic_android5);
+                break;
+
+            default:
+                return;
+        }
+        finish();
     }
 
 }
