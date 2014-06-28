3a277b874e2c671691e773c465df3e47fc6802d1
raghavlite <raghavlite@gmail.com>
2014-06-28 12:50:30 +0530

    implemented using actionbarsherlock,now many customisations available

--- src/com/example/udghosh2/MainActivity.java
+++ src/com/example/udghosh2/MainActivity.java
@@ -2,26 +2,27 @@ package com.example.udghosh2;
 
 import java.util.ArrayList;
 
+import com.actionbarsherlock.app.SherlockActivity;
 import com.devspark.sidenavigation.ISideNavigationCallback;
 import com.devspark.sidenavigation.SideNavigationView;
 import com.devspark.sidenavigation.SideNavigationView.Mode;
 
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+import com.actionbarsherlock.view.MenuInflater;
 
 
 import android.R.integer;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
+
+
+
 import android.widget.Toast;
 import android.os.Build;
 
-public class MainActivity extends ActionBarActivity implements ISideNavigationCallback{
+public class MainActivity extends SherlockActivity implements ISideNavigationCallback{
 
 	
 	private SideNavigationView sideNavigationView;
@@ -47,14 +48,8 @@ public class MainActivity extends ActionBarActivity implements ISideNavigationCa
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
 
+//
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -72,18 +67,7 @@ public class MainActivity extends ActionBarActivity implements ISideNavigationCa
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
 
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
 
     @Override
     public void onSideNavigationItemClick(int itemId) {
@@ -117,7 +101,7 @@ public class MainActivity extends ActionBarActivity implements ISideNavigationCa
             default:
                 return;
         }
-        finish();
+       // finish();
     }
 
 }
